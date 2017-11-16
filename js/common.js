680d18553ea125c8ecfde7838e4dbb16674fa127
lan_kruzer <ar-48@yandex,ru>
2017-11-16 18:32:34 +0300

    index page without adaptive

--- js/common.js
+++ js/common.js
@@ -1,3 +1,39 @@
-$(function() {
+$( document ).ready(function() {
+	$( ".slick-big-slider" ).slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		infinite: true,
+		arrows: true,
+		dots: true
+	})
 	
+	$( ".slick-slider-four" ).slick({
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		infinite: true,
+		arrows: true,
+		dots: false,
+		centerMode: true,
+		centerPadding: '5px'
+	})
+	
+	$( ".slick-slider-two" ).slick({
+		slidesToShow: 2,
+		slidesToScroll: 1,
+		infinite: true,
+		arrows: true,
+		dots: false,
+		centerMode: true,
+		centerPadding: '0px'
+	})
+	
+	$( ".slick-slider-tree" ).slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		infinite: true,
+		arrows: true,
+		dots: false,
+		centerMode: true,
+		centerPadding: '0px'
+	})
 });
