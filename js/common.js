08880ebc7e71906080eac2ac46923f69e7364035
lan_kruzer <ar-48@yandex,ru>
2017-11-21 17:33:02 +0300

    add page good-item.html

--- js/common.js
+++ js/common.js
@@ -148,4 +148,53 @@ $( document ).ready(function() {
 		$( this ).toggleClass( "active" );
 		$( this ).next( "ul" ).slideToggle(250);
 	})
+	
+	$( ".good-big-imgs" ).slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		infinite: true,
+		asNavFor: '.good-small-imgs'
+	})
+	
+	$( ".good-small-imgs" ).slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		arrows: false,
+		infinite: true,
+		focusOnSelect: true,
+		asNavFor: '.good-big-imgs'
+	})
+	
+	if(document.querySelector(".tabs-nav")) {
+		var btn_tabs_node = document.querySelectorAll(".tabs-nav li");
+		var btn_tabs = [btn_tabs_node.length];
+		var tab_items_node = document.querySelectorAll(".tabs-content li");
+		var tab_items = [tab_items_node.length];
+
+		for (var i = 0; i < btn_tabs_node.length; i++) {
+			btn_tabs[i] = btn_tabs_node.item(i);
+			tab_items[i] = tab_items_node.item(i);
+		}
+
+		btn_tabs[0].classList.add("active");
+		tab_items[0].classList.add("active");
+
+		btn_tabs.forEach(function(btn_tab, i, btn_tabs) {
+			btn_tab.addEventListener("click", function(event) {
+				if(!this.classList.contains("active")) {
+					event.preventDefault();
+					for (var y = 0; y < btn_tabs.length; y++) {
+						if(btn_tabs[y].classList.contains("active")) {
+							btn_tabs[y].classList.remove("active");
+							tab_items[y].classList.remove("active");
+						}
+					};
+					btn_tab.classList.add("active");
+					tab_items[i].classList.add("active");
+				}
+			})
+		})
+	}
+	
 });
